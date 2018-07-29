9d424f2f08fe736b1331f29c0bdd99d0483e2363
Amir <a.asadi94@gmail.com>
2018-07-29 19:18:31 +0430

    admin actions

--- book/admin.py
+++ book/admin.py
@@ -1,11 +1,36 @@
 from django.contrib import admin
 from book import models
-# Register your models here.
+
 
 admin.site.register(models.Author)
 admin.site.register(models.Translator)
 admin.site.register(models.Publisher)
-admin.site.register(models.Reservation)
+
 admin.site.register(models.Loan)
 admin.site.register(models.Book)
 admin.site.register(models.Copy)
+
+
+def reserve_to_return_Back(modeladmin,request,queryset):
+    for item in queryset:
+        person = item.person
+        book = item.book
+        item.delete()
+        models.Copy.objects.filter(id=book.id).update(status='a')
+
+
+def reserve_to_loan(modeladmin,request,queryset):
+
+    for item in queryset:
+        person = item.person
+        book = item.book
+        models.Loan(person=person, book=book).save()
+        item.delete()
+        models.Copy.objects.filter(id=book.id).update(status='o')
+
+
+class Reservations_Admin(admin.ModelAdmin):
+    list_display = ('person','book','date_reserved')
+    actions = [reserve_to_loan,reserve_to_return_Back]
+
+admin.site.register(models.Reservation,Reservations_Admin)
--- users/models.py
+++ users/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from book.models import Copy
+from book.models import Loan,Copy
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-
+from book import constants
 
 class Profile(models.Model):
 
@@ -14,12 +14,25 @@ class Profile(models.Model):
 
     # formoula
     number_of_donated_books = models.IntegerField(default=0, null=True,blank=True)
+    penalty = models.IntegerField(default=0,null=True,blank=True)
 
     # Profile
     image = models.ImageField(blank=True,upload_to='profile_image')
 
     # def __str__(self):
     #     return self.student_id
+
+    def best_students():
+        students = User.objects.all()
+        for student in students:
+            student_loans = Loan.objects.filter(person=student)
+            penalty = 0
+            for loan in student_loans:
+                delta = loan.due_back - loan.date_due
+                if delta > constants.LOAN_TIME:
+                    penalty += delta
+        pass
+
 
 
 @receiver(post_save, sender=User)
